7a405d1b8a99552db92e6da297a783b44b72f6f6
D. Hyuck Ahn <kickscar@gmail.com>
2017-10-10 15:53:48 +0900

    Initial commit

---
+++ test-mymath.py
@@ -0,0 +1,8 @@
+import sys
+# pythonpath에 경로를 동적으로 추가해주는 방법
+# sys.path.append('/bigdata/PycharmProjects/python_modules')
+import mymath
+
+# print(sys.path)
+print(mymath.add(10, 20))
+print(mymath.pi)
